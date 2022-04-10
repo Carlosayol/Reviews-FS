4363c67eae821f593c814aee7494c4dd21b537ab
carlosayol <Carlosaolar@gmail.com>
2022-04-10 10:53:32 -0500

    fix responsive design of form

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -12,4 +12,9 @@ export default makeStyles(() => ({
   heading: {
     color: 'rgb(255, 46, 99)',
   },
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: "column-reverse"
+    }
+  }
 }));
