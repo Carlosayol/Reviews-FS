1e651f19e4483ecdb2320181fe1dbebf3057cd8c
carlosayol <Carlosaolar@gmail.com>
2022-04-15 11:44:16 -0500

    setup env variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 import influencersRoutes from './routes/influencers.js'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -13,9 +14,8 @@ app.use(cors())
 
 app.use('/influencers', influencersRoutes)
 
-const CONNECTION_URI = "mongodb+srv://trendik-user:admin1234@cluster0.pyou4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 8000
 
-mongoose.connect(CONNECTION_URI)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server on port: ${PORT}`)))
   .catch((err) => console.log(err.message))
